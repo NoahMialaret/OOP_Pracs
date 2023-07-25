c2c11dff6a0321ae5c4e36b660a1209a0a71f1f1
FrenchieNoah012 <a1888605@adelaide.edu.au>
2023-07-25 10:15:20 +0930

    Made function to mean all elements in an array (1-2).

---
+++ function-1-2.cpp
@@ -0,0 +1,17 @@
+double array_mean(int array[], int n)
+{
+    if (n < 1)
+    {
+        return 0.0;
+    }
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        sum += array[i];
+    }
+
+    double mean = (double)sum / (double)n;
+
+    return mean;
+}
