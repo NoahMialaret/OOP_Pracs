13e42b61666a1edc058b47d190cbcc6883e1f415
FrenchieNoah012 <a1888605@adelaide.edu.au>
2023-07-29 10:45:16 +0930

    (1-5) Created function to print out the sum of two matrices.

---
+++ function-1-5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void print_summed(int array1[3][3],int array2[3][3])
+{
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            std::cout << array1[y][x] + array2[y][x] << " ";
+        }
+
+        std::cout << (char)8 << std::endl;
+    }
+}
