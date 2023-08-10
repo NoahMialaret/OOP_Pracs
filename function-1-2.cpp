cdc742a52006dd00372f4fade2fe5f1791c50a3a
FrenchieNoah012 <a1888605@adelaide.edu.au>
2023-08-10 16:51:27 +0930

    (1-2) Created function to initialise a PersonList given a size n.

---
+++ function-1-2.cpp
@@ -0,0 +1,16 @@
+#include "person.h"
+
+PersonList createPersonList(int n)
+{
+    PersonList pl;
+    pl.numPeople = n;
+    pl.people = new Person[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        pl.people[i].name = "Jane Doe";
+        pl.people[i].age  = 1;
+    }
+
+    return pl;
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "person.h"
+
+PersonList createPersonList(int);
+
+int main()
+{
+    int size = 10;
+
+    PersonList pl = createPersonList(size);
+
+    std::cout << "The number of people is " << pl.numPeople << std::endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout   << "Name: " << pl.people[i].name << " - Age: " 
+                    << pl.people[i].age << std::endl;
+    }
+
+    return 0;
+}
